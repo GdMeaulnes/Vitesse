2486c4f9b5f5de55cd8c21e2c1dcf6b68c4538b6
Richard DOUXAMI <richard.douxami@gmail.com>
2025-12-16 16:42:17 +0100

    Initial Commit

---
+++ Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by Richard DOUXAMI on 16/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VitesseTests/VitesseTests.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseTests.swift
+//  VitesseTests
+//
+//  Created by Richard DOUXAMI on 16/12/2025.
+//
+
+import Testing
+@testable import Vitesse
+
+struct VitesseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
